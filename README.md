e8ebf02c68c1df59785ae881f1d12d9db5e4501f
Werner Pyke <werner@Werners-MacBook-Pro.local>
2015-06-14 18:49:13 +0200

    Added comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # scripts
 A collection of simple shell scripts that I've used
+
+## resize_images.sh
+
+An OSX script to recursively resize images within a folder while retaining the modified date of the image. The script uses sips to resize.
+At the moment the script resizes each image 640px but that could be parameterized.
+
+Usage:
+`
+resize_images.sh <folder_name>
+`
--- resize_images.sh
+++ resize_images.sh
@@ -1,20 +1,21 @@
 function process_file {
     file="$1"
+
+    # Get the modified date/time of the file
     timestamp=`stat -f "%Sm" -t "%Y%m%d%H%M" "${file}"`
 
     echo "Processing ${file} with modified ${timestamp}"
 
+    # resize
     sips -Z 640 "${file}"
 
+    # set the modified date/time of the file back to what it was
     touch -t ${timestamp} "${file}"
 }
 
 folder="$1"
 
 pushd $folder
-
-
-
 
 while read -rd $'\0' file; do
     process_file "$file"
